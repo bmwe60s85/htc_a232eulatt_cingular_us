48b7c43ac6c92fe4a734b62ebad72d5094754bc6
bmwe60s85 <166263264+bmwe60s85@users.noreply.github.com>
2024-04-06 20:00:07 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 USE_CAMERA_STUB := true
 
-# inherit from the proprietary version
--include vendor/__MANUFACTURER__/__DEVICE__/BoardConfigVendor.mk
+# Inherit from the proprietary version
+-include vendor/htc/a32ul/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
@@ -13,19 +13,19 @@ TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := __DEVICE__
+TARGET_BOOTLOADER_BOARD_NAME := a32ul
 
 BOARD_KERNEL_CMDLINE := __CMDLINE__
 BOARD_KERNEL_BASE := 0x__BASE__
 BOARD_KERNEL_PAGESIZE := __PAGE_SIZE__
 
-# fix this up by examining /proc/mtd on a running device
+# Fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/__MANUFACTURER__/__DEVICE__/kernel
+TARGET_PREBUILT_KERNEL := device/htc/a32ul/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
