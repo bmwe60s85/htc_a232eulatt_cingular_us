50301580b5b2f79480998d234bdcc02142bc0519
bmwe60s85 <166263264+bmwe60s85@users.noreply.github.com>
2024-04-06 20:01:37 -0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 # Release name
-PRODUCT_RELEASE_NAME := __DEVICE__
+PRODUCT_RELEASE_NAME := a32ul
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/__MANUFACTURER__/__DEVICE__/device___DEVICE__.mk)
+$(call inherit-product, device/htc/a32ul/device_a32ul.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := __DEVICE__
-PRODUCT_NAME := cm___DEVICE__
-PRODUCT_BRAND := __MANUFACTURER__
-PRODUCT_MODEL := __DEVICE__
-PRODUCT_MANUFACTURER := __MANUFACTURER__
+PRODUCT_DEVICE := a32ul
+PRODUCT_NAME := cm_a32ul
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := a32ul
+PRODUCT_MANUFACTURER := htc
